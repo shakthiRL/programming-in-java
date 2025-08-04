997943ad5eaf21991c839a9c99f73b5fd68a51c5
shakthiRL <129866092+shakthiRL@users.noreply.github.com>
2025-08-04 17:40:16 +0530

    Add files via upload

---
+++ sript.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings: ["AI Engineer","Machine Learning Developer","Deep Learning Engineer","Web Developer","Data Scientist"],
+    typeSpeed: 100,
+    tackSpeed: 100,
+    tackDelay: 1000,
+    loop: true
+})
